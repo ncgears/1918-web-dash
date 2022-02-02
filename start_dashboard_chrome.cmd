461e169e6ebbc89bbb67e5d3645a092733c1f3e9
ncgears <1918ncgears@gmail.com>
2022-02-02 15:16:30 -0500

    Updates to dashboard layout

---
+++ start_dashboard_chrome.cmd
@@ -0,0 +1,3 @@
+@echo off
+REM Create shortcut with the following:
+"c:\program files (x86)\google\chrome\application\chrome.exe" --window-size="1024,768" --window-position="0,0" --user-data-dir="C:\FRC\Dashboard" --app=http://localhost:8888
